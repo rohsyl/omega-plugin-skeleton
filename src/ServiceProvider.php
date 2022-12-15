8be1a2a906a4cc364ee4dc3e5c2e8c8fd7789bb0
rohsyl <syzin12@gmail.com>
2022-12-15 16:51:11 +0100

    use spatie package tools

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,37 +1,34 @@
 <?php
 namespace rohsyl\OmegaPlugin\[PluginName];
 
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\ServiceProvider as SP;
 use rohsyl\OmegaCore\Utils\Common\Facades\Plugin as PluginManager;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 
-class ServiceProvider extends SP
+class ServiceProvider extends PackageServiceProvider
 {
 
-    public function register() {
-
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('[PluginName]')
+            ->hasConfigFile('[PluginName]')
+            ->hasViews('omega-plugin-[PluginName]')
+            ->hasRoute('web')
+            ->hasMigrations([
+                // list migration files here
+            ])
+            ;
     }
 
-    public function boot() {
-
-        $this->publishes([
-            __DIR__.'/../resources/views/overt' => resource_path('views/vendor/omega-plugin-[PluginName]/overt'),
-        ]);
-
-        $this->publishes([
-            __DIR__.'/../public' => public_path('vendor/omega/plugin/[PluginName]'),
-        ], 'public');
-
-        // load routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
-        // load migrations
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        // load views
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'omega-plugin-[PluginName]');
-
+    public function packageBooted()
+    {
         PluginManager::register(new Plugin());
     }
 }
